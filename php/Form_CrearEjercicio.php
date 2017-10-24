fd13b6e5170ce99c958dc6e433c44b22b9369060
Jon Jimenez <tarzanitxo@gmail.com>
2017-10-24 11:12:12 +0200

    php subir ejercicios a BD funciona PORFIN

--- php/Form_CrearEjercicio.php
+++ php/Form_CrearEjercicio.php
@@ -7,35 +7,41 @@
 	include('conectarBD.php');
 
 	//Recogemos los datos del ejercicio en variables.
-	// $sport = $_POST['exercisesport'];
-	// $category = $_POST['exercisetype'];
-	// $subcategory = $_POST['exercisesub'];
-	// $name = $_POST['exercisename'];
-	// $description = $_POST['exercisedescription'];
-	// $duration = $_POST['exercisetime'];
-	// $material = $_POST['exercisematerial'];
-	// $personmin = $_POST['minpeople'];
-	// $personmax = $_POST['maxpeople'];
-	// $url = $_POST['materialintroduced'];
-	$sport = "Balonmano";
-	$category = "Ataque 6-0";
-	$subcategory = "Juego con pivote";
-	$name = "Ataque en 6-0 juego con pivote 1";
-	$description = "hfdakshfldskhlksdjladsñjfldshflkdsjflkdshfñdsknvldskhfdslkafjhlsadfhdsñ";
-	$duration = "15:00";
-	$material = "10 balones, 4 conos";
-	$personmin = 7;
-	$personmax = 17;
-	$url = sha1("dslafsdalfkjsdflksdjfsd");
+	 $sport = $_POST['exercisesport'];
+	 $category = $_POST['exercisetype'];
+     $subcategory = $_POST['exercisesub'];
+	 $name = $_POST['exercisename'];
+	 $description = $_POST['exercisedescription'];
+	 $duration = $_POST['exercisetime'];
+	 $material = $_POST['exercisematerial'];
+	 $personmin = $_POST['exercisemin'];
+	 $personmax = $_POST['exercisemax'];
+	 $url = $_POST['materialintroduced'];
+	//$sport = "Balonmano";
+	//$category = "Ataque 6-0";
+	//$subcategory = "Juego con pivote";
+	//$name = "Ataque en 6-0 juego con pivote 1";
+	//$description = "descripcion";
+	//$duration = "15:00";
+	//$material = "10 balones, 4 conos";
+	//$personmin = 7;
+	//$personmax = 17;
+	//$url = "pass";
 	$data = array();
 
 	//Si el usuario está registrado hacemos la petición para subir el ejercicio.
 	
 	if(!isset($_SESSION['id'])){
-		$stmt = $db->prepare("INSERT INTO ejercicios (deporte, categoria, subcategoria, nombre, explicacion, duracion, material, personasmin, personasmax, foto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, )");
-		$stmt->bind_param('sssssssiis',$sport, $category, $subcategory, $name, $description, $duration, $material, $personasmin, $personasmax, $url);
+
+		$stmt = $db->prepare("INSERT INTO `ejercicios`(`deporte`, `categoria`, `subcategoria`, `nombre`, `explicacion`, `duracion`, `material`, `personasmin`, `personasmax`, `foto`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		
+		echo 'Deporte: ' .$sport;
+
+	    $stmt->bind_param('sssssssiis', $sport, $category, $subcategory, $name, $description, $duration, $material, $personmin, $personmax, $url);
+	    
 		if($stmt->execute()===false){
 			$data['estado']='error';
+
 		}else{
 			$data['estado']='subido';
 		}
