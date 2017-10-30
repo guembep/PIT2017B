2c7c13f6ca0bf18e069ae96c246372ea366d0537
Beatriz Fernandez <bea24fern@gmail.com>
2017-10-30 19:48:53 +0100

    Mezcla avances pablo y bea

--- js/login.js
+++ js/login.js
@@ -1,9 +1,20 @@
-  $('#siUser').hide();
+var sesion=Cookies.get('PHPSESSID');
+if (sesion==null){
+	$('#siUser').hide();
+}else{
+	$('#siUser').show();
+    $('#noUser').hide();
+    $('#login').hide();
+}	
 $(document).ready(function(){
   $('#entrar').click(function(){
   /*Envio de formulario */
     console.log("Carga el js");
-
+	if(($('#user').val()=="") || ($('#pass').val()=="")){
+      $('#badPass').html('<b class="text-danger">Debes introducir un email y contraseña!</b>').hide();
+      $('#badPass').fadeIn(300);
+      return false;
+	}else{
       var data = $("#form-entrar").serialize();
       $.ajax({
         type : 'POST',
@@ -15,8 +26,7 @@ $(document).ready(function(){
         success: function(data){
           estado = data['estado'];
           console.log(estado);
-        //  if(estado=="ok"){
-        if(estado=="logged"){
+          if((estado=="ok")||(estado=="logged")){ //quitar opcion logged cuando haya cerrar sesion
             $('#login').fadeOut();
             $('#siUser').show();
             $('#noUser').hide();
@@ -27,19 +37,36 @@ $(document).ready(function(){
             //$('#registrar').val('Registrando...');
             console.log("Todo deberia ir bien");
             $('#bodyNav').load('./registrado.html');
+            $("#inicio").attr("href", "http://www.easy2train.es/registrado.html")
             //  $('#logReg').load("../loginMini.html");
             /* El plan es aqui quitar el form de registro y poner un login */
-            
           }else{
             console.log("Algo no ha ido bien");
             //$("#resultado").fadeIn(1000, function(){
             //$("#resultado").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp;'+data+' !</div>');
             //$('#registrar').val("Registrar");
           //  });
+            $('#badPass').html('<b class="text-danger">¡Algo no ha ido bien!</b> <a href="#" data-target="#pwdModal" class="ml-5 alert-link" data-toggle="modal">Olvidaste tu contraseña?</a>').hide();
+            $('#badPass').fadeIn(300);
+
           }
         }
       });
       return false;
+	  }
+});
+});
+$("#frmRestablecer").submit(function(event){
+  event.preventDefault();
+  $.ajax({
+    url:'php/validaremail.php',
+    type:'post',
+    dataType:'json',
+    data:$("#frmRestablecer").serializeArray()
+  }).done(function(respuesta){
+    $("#mensaje").html(respuesta.mensaje);
+    $("#email").val('');
+  });
+});
+});
 
-});
-});
