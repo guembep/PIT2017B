e6f126487d83de283b612b446558cb66062c6893
Pablo Guembe <guembep@gmail.com>
2017-10-30 12:38:37 +0100

    Mejora login navbar con reset de pass

--- js/login.js
+++ js/login.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
   $('#entrar').click(function(){
   /*Envio de formulario */
     console.log("Carga el js");
+    console.log($('#user').val());
 
+    if(($('#user').val()=="") || ($('#pass').val()=="")){
+      $('#badPass').html('<b class="text-danger">Debes introducir un email y contraseña!</b>').hide();
+      $('#badPass').fadeIn(300);
+      return false;
+    }else{
       var data = $("#form-entrar").serialize();
       $.ajax({
         type : 'POST',
@@ -29,9 +35,12 @@ $(document).ready(function(){
             $('#bodyNav').load('./registrado.html');
             //  $('#logReg').load("../loginMini.html");
             /* El plan es aqui quitar el form de registro y poner un login */
-            
+
           }else{
             console.log("Algo no ha ido bien");
+            $('#badPass').html('<b class="text-danger">¡Algo no ha ido bien!</b> <a href="#" data-target="#pwdModal" class="ml-5 alert-link" data-toggle="modal">Olvidaste tu contraseña?</a>').hide();
+            $('#badPass').fadeIn(300);
+
             //$("#resultado").fadeIn(1000, function(){
             //$("#resultado").html('<div class="alert alert-danger"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp;'+data+' !</div>');
             //$('#registrar').val("Registrar");
@@ -40,6 +49,19 @@ $(document).ready(function(){
         }
       });
       return false;
+      }
 
+});
+$("#frmRestablecer").submit(function(event){
+  event.preventDefault();
+  $.ajax({
+    url:'php/validaremail.php',
+    type:'post',
+    dataType:'json',
+    data:$("#frmRestablecer").serializeArray()
+  }).done(function(respuesta){
+    $("#mensaje").html(respuesta.mensaje);
+    $("#email").val('');
+  });
 });
 });
