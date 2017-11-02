30b1d31d8554819758573e5ad225a906b6fc3d93
Beatriz Fernandez <bea24fern@gmail.com>
2017-11-02 10:36:46 +0100

    HTML de pestaña ejercicios

--- js/registrado.js
+++ js/registrado.js
@@ -21,7 +21,7 @@ function lista(){
 					div.textContent = data.email;
 					data.email;
 					data.id;*/
-					$("#contenido").html("Listado de ejercicios: "+response);
+					$("#contenido").html("Listado de ejercicios: <br>"+response);
 			 }),
 			 error:(function(xhr, status){
 					 console.log( "La solicitud ha fallado: " +  status);
@@ -32,25 +32,52 @@ function lista(){
 
 function ejercicios(){
 	$("#title").html("Ejercicios");
-	$("#contenido").html("listado de ejercicios"); 
-	//Cuando este listo el html de coger listado ejers
-	//$("#contenido").load("./htmlcojoejerciciophp.html"); 
-	//console.log("Añadiendo lista de ejercicios...");
+	$("#contenido").html("<br>"); 
 	
-	var nuevoejer = document.createElement( "input" );
-	nuevoejer.setAttribute("type","button");
-	nuevoejer.setAttribute("id","botonAddExercise");
-	nuevoejer.setAttribute("value","Añadir un nuevo ejercicio");
-	$("#contenido").append(nuevoejer);
+	var div= document.createElement("div");
+		div.setAttribute("class","container");
+	var div2= document.createElement("div");
+		div2.setAttribute("class","row");
+	var div3= document.createElement("div");
+		div3.setAttribute("class","col-md-4");
+	var h=document.createElement("h2");
+		h.innerHTML="Ver lista de ejercicios";
+	var enlace=document.createElement("a");
+		enlace.setAttribute("href","#");
+	var listado = document.createElement( "img" );
+		listado.setAttribute("src","./images/verejercicios.jpg");
+		listado.setAttribute("id","botonListado");
+		listado.setAttribute("alt","Ver listado de ejercicios");
+		listado.setAttribute("height","200");
+		listado.setAttribute("width","200");
+	div3.append(h);
+	enlace.append(listado);
+	div3.append(enlace);
+	div2.append(div3);
+
+	var div3crea= document.createElement("div");
+		div3crea.setAttribute("class","col-md-4");
+	var hcrea=document.createElement("h2");
+		hcrea.innerHTML="Añadir nuevo ejercicio";
+	var enlacecrea=document.createElement("a");
+		enlacecrea.setAttribute("href","#");
+	var nuevoejer = document.createElement( "img" );
+		nuevoejer.setAttribute("src","./images/creaEjercicios.png");
+		nuevoejer.setAttribute("id","botonAddExercise");
+		nuevoejer.setAttribute("alt","Añadir un nuevo ejercicio");
+		nuevoejer.setAttribute("height","200");
+	div3crea.append(hcrea);
+	enlacecrea.append(nuevoejer);
+	div3crea.append(enlacecrea);
+	div2.append(div3crea);
+	div.append(div2);
+	$("#contenido").append(div);
+	
+
 	$("#botonAddExercise").click(function (){console.log("acceder a añadir ejercicio"); 
 		nuevoejercicio();
 		});
 		
-	var listado = document.createElement( "input" );
-	listado.setAttribute("type","button");
-	listado.setAttribute("id","botonListado");
-	listado.setAttribute("value","Ver listado de ejercicios");
-	$("#contenido").append(listado);
 	$("#botonListado").click(function (){
 		console.log("acceder a lista de ejercicios"); 
 		lista();	
