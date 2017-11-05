427e07e08e01f4bed9fcc41499b7f0367332030a
Beatriz Fernandez <bea24fern@gmail.com>
2017-11-05 19:18:59 +0100

    html despues de login v1

--- js/registrado.js
+++ js/registrado.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$("#linkejercicios").click(function (){console.log("acceder a ejercicios"); ejercicios();});
 	$("#linkconvocatorias").click(function (){console.log("acceder a convocatorias"); convocatorias();});
 
