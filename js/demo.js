51fb857c781de2cebfe0a5181416be80549026ca
Beatriz Fernandez <bea24fern@gmail.com>
2017-11-22 13:33:19 +0100

    Redireccion después de registro en popup

--- js/demo.js
+++ js/demo.js
@@ -2,10 +2,7 @@ $("#demoadd").click(function (){
 	$('#iframeregistrar').contents().find('body').find(".navbar").remove();
 	$('#iframeregistrar').contents().find('body').css('padding-top',0);
 	$('#iframeregistrar').contents().find('body').find('#registrar').click(function(){
-		//si el registro ha sido correcto 
-		//if()
-		console.log("cerrar popup");
-		//$("#popupDemo").dialog('close');
+		parent.location.href = 'https://easy2train.es';
 	});
 });
 
